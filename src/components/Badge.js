5b660289ae31639d0a332c8d7267723697d403e4
Kevin Santacruz <defonix0@gmail.com>
2019-12-31 17:33:37 -0500

    badge component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import confLogo from '../images/badge-header.svg';
+import './styles/Badge.css';
+
 class Badge extends React.Component {
 
     render() {
-        return (<div>
-            <div>
+        return (
+        <div className="Badge">
+            <div className="Badge__header">
                 <img src={confLogo} alt="Conferencia"></img>
             </div>
-            <div>
-                <img src="https://gravatar.com/avatar?id=identicon" alt="Avatar"></img>
+            <div className="Badge__section-name">
+                <img className="Badge__avatar" src="https://gravatar.com/avatar?id=identicon" alt="Avatar"></img>
                 <h1>Kevin
                 <br></br>
                 Santacruz
                 </h1>
             </div>
-            <div>
+            <div className="Badge__section-info">
                 <p>Frontend Enginner</p>
                 <p>@ksantacr_</p>
             </div>
-            <div>#platziconf</div>
+            <div className="Badge__footer">#platziconf</div>
 
         </div>);
     }
